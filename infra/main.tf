2280e5b39e1be494a81509853fbb6c3250663571
Ronan O'Dea <L00203120@atu.ie>
2026-03-22 20:05:18 +0000

    Updated OpenTofu provider config, add resource provider registration - plan succesful, so will merge to main and revisit when ready to deploy

--- infra/main.tf
+++ infra/main.tf
@@ -11,7 +11,16 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
+  subscription_id                = var.subscription_id
+  resource_provider_registrations = "none"
+}
+
+resource "azurerm_resource_provider_registration" "compute" {
+  name = "Microsoft.Compute"
+}
+
+resource "azurerm_resource_provider_registration" "network" {
+  name = "Microsoft.Network"
 }
 
 resource "azurerm_resource_group" "a11y" {
